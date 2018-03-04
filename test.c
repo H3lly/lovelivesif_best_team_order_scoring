25b53472470ecd8ff27b3ef1551586cdd9796c27
H3lly <h3lly.h0lly@gmail.com>
2018-03-04 11:47:05 +0100

    Main completion

--- test.c
+++ test.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int typeCmp(char *type, char *user_input){
-	if(strcmp(type, user_input)==0)
-		return 0;
-	else{
-		fprintf("Wrong input. Please input Cool, Smile or Pure : \n")
+/*int typeCmp(char *type, char *user_input){
+	if(strcmp(type, user_input)!=0){
+		fprintf("Wrong input. Please input Cool, Smile or Pure : \n");
 		return 1;
+	}else {
+		//faire la comparaison
 	}
 }
 
-int isEqual(char *a, char* b){
-	
+//int isEqual(char *a, char* b){
+
 }
 int inputIsValid(int i){
 	char **tab;
@@ -25,13 +25,18 @@ int inputIsValid(int i){
 	tab[6] = "PURE";
 	tab[7] = "PUre";
 
-}
+}*/
 
 int main(void){
-	char *type = malloc(5*sizeof(char));
-	puts("Chose Cool, Smile or Pure: \n");
-	scanf("%s", type);
-	while(strcmp)
-	printf("Type choosen : %s\n", type);
+	int res = 0;
+	puts("Chose Cool (1), Smile (2) or Pure (3):");
+	scanf("%i", &res);
+	if(res<1 || res>3){
+		while(res<1 || res>3){
+			puts("Wrong input. Please write the corresponding number - Cool is 1, Smile is 2 and Pure is 3:");
+			scanf("%i", &res);
+		}
+	}
+	printf("Type choosen : %i\n", res);
 	return 0;
 }
